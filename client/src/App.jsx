839fa76db19b9d9516a7814a60a13c6457efc8a2
nithish <nithish6@gmail.com>
2024-07-10 17:16:09 -0700

    completed signup page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Signin from './pages/SignIn'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import Header from './components/Header'
+import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<SignUp />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/about' element={<About />} />
       </Routes>
