29682e9e5eabbcf651ce6330e7b640ffa74f60ef
nithish <nithish6@gmail.com>
2024-07-15 17:35:29 -0700

    completed signin page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ function Header() {
                     <Link to='/about'>
                         <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                     </Link>
-                    <Link to='/sign-in'>
+                    <Link to='/signin'>
                         <li className='hidden sm:inline text-slate-700 hover:underline'>Sign in</li>
                     </Link>
                 </ul>
